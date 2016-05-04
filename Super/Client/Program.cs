71e60aa8be3e8df4ad2dd509553689cd3485ce05
skyish <cristiano_rosario@msn.com>
2016-05-04 21:25:22 +0100

    added project files

---
+++ Super/Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Server;
+
+namespace Client
+{
+    class MyCxMsg : MarshalByRefObject, ICxMsg
+    {
+        public void AcceptMsg(string id, string msg)
+        {
+            Console.WriteLine("\n\n\n #####################");
+            Console.WriteLine("Remetente => " + id);
+            Console.WriteLine("Mensagem => " + msg);
+            Console.WriteLine("##################### \n\n\n ");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCxMsg msb = new MyCxMsg();
+
+            IServer server = (IServer)Activator.GetObject(typeof(IServer), "http://localhost:8888");
+            server.Register("Skyish", msb);
+            server.SendMsg("Skyish", "Hello World");
+        }
+    }
+}
---
+++ Super/Server/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server;
+
+
+namespace Server
+{
+
+    class Server : MarshalByRefObject, IServer
+    {
+
+        Dictionary<string, ICxMsg> mailBoxes = new Dictionary<string, ICxMsg>();
+
+    
+        public void Register(string id, ICxMsg mb)
+        {
+            mailBoxes.Add(id, mb);
+        }
+
+        public void Unregister(string id, ICxMsg mb)
+        {
+            mailBoxes.Remove(id);
+        }
+
+        public void SendMsg(string id, string msg)
+        {
+            foreach(string key in mailBoxes.Keys)
+            {
+                mailBoxes[key].AcceptMsg(id, msg);
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
